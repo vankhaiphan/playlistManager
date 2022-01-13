068e53d97359ba97c4964947e6d490d00c023175
vankhaiphan <pvkhai98dn@gmail.com>
2022-01-13 03:10:33 +0100

    change package

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-//Install express server
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 const express = require('express');
-const path = require('path');
-
 const app = express();
+app.use(requireHTTPS);
+app.use(express.static("./dist/playlist-manager"));
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/playlist-manager'));
-
-app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/dist/playlist-manager/index.html'));
+app.get("/*", function(req, res) {
+    res.sendFile("index.html", {root: "dist/playlist-manager/"}
+  );
 });
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
